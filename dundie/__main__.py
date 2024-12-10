ab37e3195f4755325cba5e1c41192b1e83ba4b39
Marcelo <marcelo.cavalcanti@mv.com.br>
2024-12-10 11:36:54 -0300

    criado pasta de docs setup e inicializaçõa

---
+++ dundie/__main__.py
@@ -0,0 +1,1 @@
+print("execuing entry point for dundie...")
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "dundie",
+    version = "0.1.0",
+    description = "Rewards Point System for Dunder Mifflin",
+    author = "Marcelo de Moura",
+    packages = find_packages(),
+    include_package_data=True,
+    install_requires=[],
+)
