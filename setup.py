a96540c5d1f22112054a3219ecbbf64d5f6748b0
Marcelo <marcelo.cavalcanti@mv.com.br>
2024-12-10 13:49:24 -0300

    setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,17 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name = "dundie",
-    version = "0.1.0",
-    description = "Rewards Point System for Dunder Mifflin",
-    author = "Marcelo de Moura",
-    packages = find_packages(),
+    name="dundie",
+    version="0.1.0",
+    description="Rewards Point System for Dunder Mifflin",
+    author="Marcelo de Moura",
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[],
+    entry_points={
+        "console_scripts":[
+            "dundie = dundie.__main__"
+        ]
+    }
+    
 )
